f09755ad221f41b4387096f3bc906a5732e46f0d
daikitakaki <162309554+takakidaiki@users.noreply.github.com>
2024-06-13 21:54:14 +0000

    4

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,23 +2,31 @@ class ReviewsController < ApplicationController
   def new
     @review = Review.new
   end
-  
+
   def create
     @review = Review.new(review_params)
     @review.user_id = current_user.id
     @review.save
-    redirect_to review_path
+    redirect_to reviews_path
   end
 
   def index
+    @reviews = Review.all
   end
 
   def show
+    @review = Review.find(params[:id])
   end
 
   def edit
   end
   
+  def destroy
+    review = Review.find(params[:id])
+    review.destroy
+    redirect_to '/reviews'
+  end
+
   private
 
   def review_params
