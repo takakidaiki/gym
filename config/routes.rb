cf4cc87569ecd8be7bc190c942e8ddbd8e08c3cf
daikitakaki <162309554+takakidaiki@users.noreply.github.com>
2024-06-14 10:14:23 +0000

    [finish]4

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  get 'users/unsubscribe'
+  get 'users/withdraw'
   devise_for :users
   root to: "homes#top"
 
   resources :reviews, only: [:new, :create, :index, :show, :edit, :destroy]
+  resources :users, only: [:show, :index, :edit]
 
   get "/homes/about" => "homes#about", as: "about"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
