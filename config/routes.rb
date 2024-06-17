3df0491018957c5b9f4b4afc5a5e94e2f2a319c1
daikitakaki <162309554+takakidaiki@users.noreply.github.com>
2024-06-17 12:45:50 +0000

    5

--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,9 @@ Rails.application.routes.draw do
 
   resources :reviews, only: [:new, :create, :index, :show, :edit, :destroy] do
     resources :review_comments, only: [:create, :destroy]
+    resource :favorite, only: [:create, :destroy]
   end
   resources :users, only: [:show, :index, :edit, :update]
-  resource :favorite, only: [:create, :destroy]    
 
   get "/homes/about" => "homes#about", as: "about"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
